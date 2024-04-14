61d96e27b586f63b9776dae2b0ab6f0452cd4448
Ireukalla <Ireukalla@gmail.com>
2024-04-15 01:15:58 +0900

    Code updated

---
+++ index.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+const input = () => {
+  const input = fs.readFileSync('/dev/stdin').toString()
+  const tokens = input.split('\n').flatMap((token) => token.split(' '))
+  const validTokens = tokens.filter((token) => token !== '')
+
+  return validTokens
+}
+
+const TOKENS = input()
+let TOKEN_INDEX = 0
+
+const scanString = () => {
+  const stringValue = TOKENS[TOKEN_INDEX]
+  TOKEN_INDEX += 1
+  return stringValue
+}
+
+const scanInt = () => {
+  const intValue = parseInt(TOKENS[TOKEN_INDEX])
+  TOKEN_INDEX += 1
+  return intValue
+}
+
+// -------------------------------------------- CODE -------------------------------------------- //
